5091f39c6785faa1a4063e340e1d8b10e85f1342
tuadmin <tuadmin@users.noreply.github.com>
2022-07-23 05:13:44 +0000

    configuraqr entorno

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+sudo su
+apt-get update
 pecl install openswoole
+echo "extension=openswoole.so" >> "/usr/local/etc/php/conf.d/swoole.ini"
+composer require --dev swoole/ide-helper:@dev
